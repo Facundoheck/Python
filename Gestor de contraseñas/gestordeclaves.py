9ccd59a231ae81c8fe02ad8be1a0162eeb159bde
Facundoheck <heckfacundo9@gmail.com>
2023-08-15 18:00:53 -0300

    funcion que exporta a json resuelta

--- Gestor de contraseñas/gestordeclaves.py
+++ Gestor de contraseñas/gestordeclaves.py
@@ -1,14 +1,10 @@
 import json
 
-#inicializar diccionario
-diccionario={}
-
 #constructor de la clase gestor
 class Usuario:
-    def __init__(self,user,psw,desc):
+    def __init__(self,user,psw):
         self.user=user
         self.psw=psw
-        self.desc=desc
     
     def toDict(self):
         return {
@@ -24,62 +20,76 @@ class UserManager:
     def __init__(self):
         self.users = {}
 
-#defino nuevas funciones para el gestor
-def nuevoRegistro(self,user,psw,desc):
-    user=input('Usuario: ')
-    psw=input('Constraseña: ')
-    desc=input('Descripcion: ')
-    if user not in self.users:
-        self.users[user]=Usuario(user,psw,desc)
-        print('*'*30)
-        print('Nuevo usuario añadido con éxito')
-        print('*'*30)
-    else:
-        print('El usuario ya existe')
+    def nuevoUsuario(self,user,psw):
+        if user not in self.users:
+            self.users[user]=Usuario(user,psw)
+            print('*'*30)
+            print('Nuevo usuario añadido con éxito')
+            print('*'*30)
+        else:
+            print('*'*30)
+            print('El usuario ya existe')
+            print('*'*30)
 
-def actualizarContraseña(self,user,nueva_psw):
-    user=input('Usuario: ')
-    if user == self.users.get(user):
-        nueva_psw=input('Nueva contraseña: ')
-        self.users.psw=nueva_psw
-        print('*'*30)
-        print('Clave actualizada con éxito')
-        print('*'*30)
+    def actualizarContraseña(self,user,nueva_psw):
+        if user == self.users.get(user):
+            self.users.psw=nueva_psw
+            print('*'*30)
+            print('Clave actualizada con éxito')
+            print('*'*30)
 
-def actualizarUsuario(self,user):
-    user=input('Usuario: ')
-    if user==self.users.get(user):
-        nuevo_user=('Ingrese nuevo usuario: ')
-        self.users.user=nuevo_user
-        print('*'*30)
-        print('Usuario actualizado con éxito')
-        print('*'*30)
-    else:
-        print('El usuario ingresado no existe')
+    def actualizarUsuario(self,user,nuevo_user):
+        if user==self.users.get(user):
+            self.users.user=nuevo_user
+            print('*'*30)
+            print('Usuario actualizado con éxito')
+            print('*'*30)
+        else:
+            print('*'*30)
+            print('El usuario ingresado no existe')
+            print('*'*30)
 
-def verId(self,user):
-    user=input('Usuario: ')
-    if self.users.user==user:
-        print('*'*30)
-        print('Usuario: ',self.users.user)
-        print('Clave: ',self.users.psw)
-        print('Descripcion: ',self.users.desc)
-        print('*'*30)
+    def listaUsuario(self):
+        print("Usuarios registrados:")
+        for username, user in self.users.items():
+            print('*'*50)
+            print(f"Usuario: {username}, Contraseña: {user.psw}")
+            print('*'*50)
 
-""" def opciones():
+    def toJson(self, filename):
+            user_list = [user.to_dict() for user in self.users.values()]
+            with open(filename, 'w') as file:
+                json.dump(user_list, file)
+                print('*'*30)
+                print("Registros exportados a", filename)
+                print('*'*30)
+def main():
+    manager = UserManager()
+
     while True:
-        opcion=input('a. Agregar un usuario y una clave\nb. Actualizar clave\nc. Actualizar usuario\nd. Ver usuario y clave\ne. Salir\nIngrese opcion: ')
-        if opcion=='a':
-            nuevoRegistro()
-        elif opcion=='b':
-            actualizarContraseña(diccionario)
-        elif opcion=='c':
-            actualizarUsuario(diccionario)
-        elif opcion=='d':
-            verId(diccionario)
-        elif opcion=='e':
+        opcion=input('1. Agregar un usuario y una clave\n2. Actualizar clave\n3. Actualizar usuario\n4. Ver usuario y clave\n5. Salir\n6. Exportar datos a JSON\nIngrese opcion: ')
+        if opcion=='1':
+            user=input('Usuario: ')
+            psw=input('Constraseña: ')
+            manager.nuevoUsuario(user,psw)
+        elif opcion=='2':
+            user=input('Usuario: ')
+            nueva_psw=input('Nueva contraseña: ')
+            manager.actualizarContraseña(user,nueva_psw)
+        elif opcion=='3':
+            user=input('Usuario: ')
+            nuevo_user=('Ingrese nuevo usuario: ')
+            manager.actualizarUsuario(user,nuevo_user)
+        elif opcion=='4':
+            manager.listaUsuario()
+        elif opcion=='5':
+            filename=input('Ingrese el nombre del archivo: ')
+            manager.toJson(filename)
+        elif opcion=='6':
             break
         else:
             print('Opción incorrecta, intente nuevamente')
- """
-opciones()
+
+if __name__ == "__main__":
+    main()
+
